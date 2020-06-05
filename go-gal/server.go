4cfa9d99022a7481830189489f3219769995431c
Eryb <eryb@protonmail.com>
2020-06-05 16:42:14 +0530

    Remove tracker log

--- go-gal/server.go
+++ go-gal/server.go
@@ -25,23 +25,12 @@ func (s *Server) Run(h http.Handler) {
 	fmt.Println(s.Message())
 
 	log.Printf("Listening at %s", s.Address())
-	log.Printf("Tracker - %s", s.Tracker())
-
 	log.Fatal(http.ListenAndServe(s.Address(), h))
 }
 
 // Address returns formatted hostname and port
 func (s *Server) Address() string {
 	return fmt.Sprintf("%s:%s", s.Hostname, s.HTTPPort)
-}
-
-// Tracker returns the tracker link to be used for event collection
-func (s *Server) Tracker() string {
-	return fmt.Sprintf(
-		`http://%s:%s/knock-knock`,
-		s.Hostname,
-		s.HTTPPort,
-	)
 }
 
 // Message is the server start message
